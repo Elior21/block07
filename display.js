a1733630f9682db2ab79b884ef54cf82b0857b1a
elior valenzuela <eliorvalenzuela@gmail.com>
2023-06-27 15:46:17 -0400

    first commit

---
+++ display.js
@@ -0,0 +1,13 @@
+
+
+
+const userName = prompt("Please enter your name ");
+
+alert("your name is " + userName)
+
+const threeNum = prompt ("please enter three numbers")
+
+alert("please confirm your numbers " + threeNum)
+
+alert(`thank you ${userName} your combination is ${threeNum}`)
+
---
+++ vault.js
@@ -0,0 +1,8 @@
+//pseudocode create arithmantic variables and create alert
+
+let num1 = 5 * 2
+let num2 = 20 + 20
+let num3 = 45 - 6
+
+alert(`You have received this message because you have been chosen to open an important vault. 
+Here is the  secret combination; ${num1} - ${num2} - ${num3}`)
